8ac57aa120ffd0f369ee82b7e43133393f617d97
Pratiksha <pratikshakale7776@gmail.com>
2024-08-07 08:10:28 +0530

    -

---
+++ ARRAY/arrayMethods.js
@@ -0,0 +1,37 @@
+let oldArr=["banana" ,"apple","orange","mango","kiwi"];
+let oldArr1=["banana1" ,"apple1","orange1","mango1","kiwi1"];
+let oldArr2=["banana2" ,"apple2","orange2","mango2","kiwi2"];
+
+// console.log(oldArr.length);
+//------------------------------
+// let newArr=oldArr.toString()
+// console.log(newArr);
+//-------------------------------
+// console.log(oldArr.at(1));
+//--------------------------
+// console.log(oldArr.join('*'));
+//-----------------------------
+//  const popval=oldArr.pop();
+//  console.log(popval);
+//----------------------------
+// oldArr.push('bitroot');
+// console.log(oldArr);
+//--------------------------
+// oldArr.shift();
+// console.log(oldArr);
+//--------------------------
+// oldArr.unshift('stowberi');
+// console.log(oldArr);
+//---------------------------
+
+// let newa=oldArr.concat(oldArr1);
+// console.log(typeof(newa));
+// console.log(newa);
+//---------------------
+// console.log(oldArr.reverse());
+//---------------------------------
+
+// let oldArr0=oldArr.copyWithin(2,4)
+// console.log(oldArr0);
+//--------------------------------
+
